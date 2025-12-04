e56b4b799e36e9275666d4c89c470d8f2aacbbeb
TheBangOut758 <odjoudi8@gmail.com>
2025-12-04 17:37:55 +0000

    the Parse File

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,5 +1,22 @@
-module Parse where
+{-# LANGUAGE OverloadedStrings #-}
 
-parseData :: String -> IO ()
-parseData _ =
-    putStrLn "Parsing placeholder – real JSON decoding coming soon."
+module Parse
+  ( parseCrimes
+  , writeCrimes
+  ) where
+
+import Types
+
+import Data.Aeson
+  ( eitherDecode
+  , encode
+  )
+import qualified Data.ByteString.Lazy as B
+
+
+parseCrimes :: B.ByteString -> Either String [Crime]
+parseCrimes = eitherDecode
+
+
+writeCrimes :: FilePath -> [Crime] -> IO ()
+writeCrimes fp crimes = B.writeFile fp (encode crimes)
